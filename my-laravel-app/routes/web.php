21a71637cf418e39b99efb1988290ffaaf48a1fa
satoru_kakinohana <satoru_kakinohana@bwave.co.jp>
2021-04-14 14:58:59 +0900

    a

--- my-laravel-app/routes/web.php
+++ my-laravel-app/routes/web.php
@@ -42,3 +42,48 @@ Route::get('song/', [SongController::class, 'index']);
 Route::get('song/sampleMethod01/', [SongController::class, 'sampleMethod01']);
 Route::get('song/addmylist/{song_id}', [SongController::class, 'addmylist']);
 
+
+
+//============================================================================================
+//
+//============================================================================================
+Route::get('/question01/challenge_users/save/', function () {
+    return redirect('/question01/winners');
+});
+
+
+// 先頭「api」をまとめて書く場合、こんな感じ
+Route::group(['namespace' => 'API'], function () {
+    Route::get('call/me', function () {return 'GET:call/me';});  // http://localhost:8000/call/me
+    Route::post('call/me', function () {return 'POST:call';});
+    Route::get('challenge_users', function () {return 'GET:challenge_users';});   // http://localhost:8000/challenge_users
+    Route::post('challenge_users', function (){return 'POST:challenge_users';});
+});
+
+
+// 先頭「api」をまとめて書き、先頭にプレフィックスを付ける場合、こんな感じ。
+Route::group(['namespace' => 'API'], function () {
+    //--------------------------------
+    //         Question 01
+    //--------------------------------
+    Route::prefix('q01')->group(function () {
+        Route::get('call/me', function () {return 'GET:q01/call/me';});  // http://localhost:8000/q01/call/me
+        Route::post('call/me', function () {return 'POST:q01/call/me';});
+        Route::get('challenge_users', function () {return 'GET:q01/challenge_users';});
+        Route::post('challenge_users', function () {return 'POST:q01/challenge_users';});
+    });
+
+    //--------------------------------
+    //         Question 02
+    //--------------------------------
+    Route::prefix('q02')->group(function () {
+        Route::get('call/my/APIs'    , function () {return 'GET:q02/call/my/APIs';});
+        Route::post('call/my/APIs'   , function () {return 'POST:q02/call/my/APIs';});
+        Route::put('call/my/APIs'    , function () {return 'PUT:q02/call/my/APIs';});
+        Route::patch('call/my/APIs'  , function () {return 'PATCH:q02/call/my/APIs';});
+        Route::delete('call/my/APIs' , function () {return 'DELETE:q02/call/my/APIs';});
+        Route::options('call/my/APIs', function () {return 'OPTIONS:q02/call/my/APIs';});
+    });
+
+});
+
